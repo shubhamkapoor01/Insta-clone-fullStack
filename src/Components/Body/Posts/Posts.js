4094aa89ee5c914b89fef2fa9ae182b444725572
Shubham Kapoor <shubham.kapoor01@gmail.com>
2021-09-12 15:27:16 +0530

    commit

--- src/Components/Body/Posts/Posts.js
+++ src/Components/Body/Posts/Posts.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Posts.css'
 import { useStateValue } from '../../../StateProvider'
+import db from '../../../Firebase/firebase'
+import { render } from '@testing-library/react';
 
 function Posts() {
+	const [{ user }] = useStateValue();
+	const [{ users }] = useStateValue();
 	const [{ posts }, dispatch] = useStateValue();
+
+	const addLike = async (post) => {
+		if (post.likedBy.includes(user.email)) {
+			const idx = post.likedBy.indexOf(user.email)
+			post.likedBy.splice(idx, 1);
+			post.likes --;
+			db.collection("posts").doc(post.id).set({
+				id: post.id,
+				authorName: post.authorName,
+				authorEmail: post.authorEmail,
+				likes: post.likes,
+				image: post.image,
+				caption: post.caption,
+				comments: post.comments,
+				likedBy: post.likedBy,
+			});
+		} else {
+			post.likes ++;
+			post.likedBy = [...post.likedBy, user.email];
+			db.collection("posts").doc(post.id).set({
+				id: post.id,
+				authorName: post.authorName,
+				authorEmail: post.authorEmail,
+				likes: post.likes,
+				image: post.image,
+				caption: post.caption,
+				comments: post.comments,
+				likedBy: post.likedBy,
+			});
+		}
+		render();
+		return;
+	}
 
 	return (
 		<div className="posts">
@@ -17,9 +54,16 @@ function Posts() {
 							src={ post.image } 
 							alt="Could not load.. refresh to retry.">
 						</img>
-						<p className="post__caption">
-							{ post.caption }
-						</p>
+						<div className="post__footer">
+							<div className="post__caption">
+								{ post.caption }
+							</div>
+							<div className="button__div"> 
+								<button className="post__likes" onClick={ (e) => addLike(post) }>
+									Likes: { post.likes }
+								</button>
+							</div>
+						</div>
 					</div>
 				)
 			})}
